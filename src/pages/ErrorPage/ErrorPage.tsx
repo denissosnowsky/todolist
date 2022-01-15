83016cadbd6ebc774c25732da43f1e760c540ede
Denys Sosnovskyi <denissosnowsky@gmail.com>
2022-01-15 14:24:26 +0200

    made error-page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,7 @@
+import s from "./ErrorPage.module.css";
+
 const ErrorPage: React.FC = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return <div className={s.wrapper}>Error 404</div>;
+};
 
 export default ErrorPage;
